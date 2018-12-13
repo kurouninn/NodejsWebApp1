c88db45d9a2d64cf3bd2990a60a610166022187f
一戸 善弘 <yi@hus.ac.jp>
2018-12-13 15:54:27 +0900

    0.0

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,8 +1,37 @@
 'use strict';
 var http = require('http');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
+var html = null;
+
+html = fs.readFileSync('index.html', 'utf8');
+
+
+var rpio = require('rpio');
+//const pin0 = 0;
+//var flg = false;
+
+//rpio.open(pin0, rpio.INPUT);
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+
+    if (req.method == 'POST') {
+        //req.on('data', function (chunk) {
+        //    //rpio.write(pin0, rpio.HIGH);
+        //    //rpio.write(pin0, rpio.LOW);
+        //});
+    } else if (req.method == 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(html);
+        res.end();
+        //if (flg) {
+        //    rpio.write(pin0, rpio.LOW);
+        //    flg = false;
+        //} else {
+        //    rpio.write(pin0, rpio.HIGH);
+        //    flg = true;
+        //}
+    }
+
 }).listen(port);
+
