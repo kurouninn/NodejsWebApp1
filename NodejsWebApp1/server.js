7e8fc827c3e36de75f585a367d2e5d87b8082fd8
一戸 善弘 <yi@hus.ac.jp>
2018-12-14 11:04:08 +0900

    1

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,6 +1,8 @@
 'use strict';
 var http = require('http');
 var fs = require('fs');
+var exec = require('child_process').exec;
+var child = null;
 var port = process.env.PORT || 1337;
 var html = null;
 
@@ -8,18 +10,28 @@ html = fs.readFileSync('index.html', 'utf8');
 
 
 var rpio = require('rpio');
-//const pin0 = 0;
-//var flg = false;
 
-//rpio.open(pin0, rpio.INPUT);
+const pin0 = 0;
+var flg = false;
+
+rpio.open(pin0, rpio.INPUT);
+rpio.write(pin0, rpio.HIGH);
 
 http.createServer(function (req, res) {
 
     if (req.method == 'POST') {
-        //req.on('data', function (chunk) {
-        //    //rpio.write(pin0, rpio.HIGH);
-        //    //rpio.write(pin0, rpio.LOW);
-        //});
+        req.on('data', function (chunk) {
+            var data = '' + chunk;
+            var d = data.split('=');
+            console.log(d[0]);
+            child = exec('ls', (err, stdout, stderr) => {
+                console.log('stdo' + stdout + '\n');
+                console.log('stde' + stderr + '\n');
+            });
+
+            //rpio.write(pin0, rpio.HIGH);
+            //rpio.write(pin0, rpio.LOW);
+        });
     } else if (req.method == 'GET') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(html);
@@ -35,3 +47,26 @@ http.createServer(function (req, res) {
 
 }).listen(port);
 
+function led(led) {
+    if (led == 'blue') {
+        if (child != null) child.kill('SIGHUP');
+        child = null;
+        child = exec('./test1 1', (err, stdout, stderr) => { });
+    } else if (led == 'red') {
+        if (child != null) child.kill('SIGHUP');
+        child = null;
+        exec('./test1 2', (err, stdout, stderr) => { });
+    } else if (led == 'green') {
+        if (child != null) child.kill('SIGHUP');
+        child = null;
+        exec('./test1 3', (err, stdout, stderr) => { });
+    } else if (led == 'blink') {
+        if (child != null) child.kill('SIGHUP');
+        child = null;
+        exec('./test1 4', (err, stdout, stderr) => { });
+    } else if (led == 'fade') {
+        if (child != null) child.kill('SIGHUP');
+        child = null;
+        exec('./test1 5', (err, stdout, stderr) => { });
+    }
+}
