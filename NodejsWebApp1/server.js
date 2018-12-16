c236d83a0c6877151fc8f6e5e07d27e8aeabda22
kurouninn <kurouninn2003@hotmail.com>
2018-12-16 13:16:40 +0900

    1

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -9,11 +9,11 @@ var gpio_rgb = {r:0, g:0, b:0};
 
 html = fs.readFileSync('index.html', 'utf8');
 
-var gpio_pin = { b: 11, r: 12, g: 13, b_on: false, r_on: false, g_on: false }
+var gpio_pin = { b: 11, r: 12, g: 13, b_on: false, r_on: false, g_on: false };
 var gpio = require('rpi-gpio');
-gpio.setup(gpio_pin.b, gpio.DIR_OUT, (err) => { console.log('b pin err ' + err + '\n'); });
-gpio.setup(gpio_pin.r, gpio.DIR_OUT, (err) => { console.log('r pin err ' + err + '\n'); });
-gpio.setup(gpio_pin.g, gpio.DIR_OUT, (err) => { console.log('g pin err ' + err + '\n'); });
+gpio.setup(gpio_pin.b, gpio.DIR_OUT, (err) => { console.log('b pin err ' + err); });
+gpio.setup(gpio_pin.r, gpio.DIR_OUT, (err) => { console.log('r pin err ' + err); });
+gpio.setup(gpio_pin.g, gpio.DIR_OUT, (err) => { console.log('g pin err ' + err); });
 
 process.on('SIGTERM', () => {
     console.log('get SIGTERM.\n');
@@ -30,16 +30,16 @@ http.createServer(function (req, res) {
             console.log(d[0]);
             if (d[0] == 'blue') {
                 gpio_pin.b_on = !gpio_pin.b_on;
-                console.log('b pin ' + gpio_pin.b_on + '\n');
                 gpio.write(gpio_pin.b, gpio_pin.b_on);
+                console.log('b pin ' + gpio_pin.b_on);
             } else if (d[0] = 'red') {
                 gpio_pin.r_on = !gpio_pin.r_on;
-                console.log('b pin ' + gpio_pin.r_on + '\n');
                 gpio.write(gpio_pin.r, gpio_pin.r_on);
+                console.log('r pin ' + gpio_pin.r_on);
             } else if (d[0] = 'green') {
                 gpio_pin.g_on = !gpio_pin.g_on;
-                console.log('b pin ' + gpio_pin.g_on + '\n');
                 gpio.write(gpio_pin.g, gpio_pin.g_on);
+                console.log('g pin ' + gpio_pin.g_on);
             } else if (d[0] = 'blink') {
 
             } else if (d[0] = 'fade') {
