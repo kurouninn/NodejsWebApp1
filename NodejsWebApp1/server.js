184149a4976c5c5cefd46fbeceb488e9d70c3387
kurouninn <kurouninn2003@hotmail.com>
2018-12-16 13:46:54 +0900

    1

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -9,8 +9,57 @@ var gpio_rgb = {r:0, g:0, b:0};
 
 html = fs.readFileSync('index.html', 'utf8');
 
-var gpio_pin = { b: 11, r: 12, g: 13, b_on: false, r_on: false, g_on: false };
+var gpio_pin = { b: 11, g: 12, r: 13, b_on: 0, r_on: 0, g_on: 0 };
+var gpio = require('rpio');
+
+gpio.open(gpio_pin.b, gpio.OUTPUT);
+gpio.open(gpio_pin.g, gpio.OUTPUT);
+gpio.open(gpio_pin.r, gpio.OUTPUT);
+
+process.on('SIGTERM', () => {
+    console.log('get SIGTERM.\n');
+    gpio.write(gpio_pin.b, 0);
+    gpio.write(gpio_pin.r, 0);
+    gpio.write(gpio_pin.g, 0);
+});
+
+http.createServer(function (req, res) {
+    if (req.method == 'POST') {
+        req.on('data', function (chunk) {
+            var data = '' + chunk;
+            var d = data.split('=');
+            console.log(d[0]);
+            if (d[0] == 'blue') {
+                gpio_pin.b_on = 1 - gpio_pin.b_on;
+                gpio.write(gpio_pin.b, gpio_pin.b_on);
+                console.log('b pin ' + gpio_pin.b_on);
+            } else if (d[0] == 'red') {
+                gpio_pin.r_on = 1 - gpio_pin.r_on;
+                gpio.write(gpio_pin.r, gpio_pin.r_on);
+                console.log('r pin ' + gpio_pin.r_on);
+            } else if (d[0] == 'green') {
+                gpio_pin.g_on = 1 - gpio_pin.g_on;
+                gpio.write(gpio_pin.g, gpio_pin.g_on);
+                console.log('g pin ' + gpio_pin.g_on);
+            } else if (d[0] == 'blink') {
+
+            } else if (d[0] == 'fade') {
+
+            } else {
+                gpiop.setup(gpio_pin.b, gpio.DIR_OUT).then(() => { });
+            }
+        });
+    } else if (req.method == 'GET') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(html);
+        res.end();
+    }
+}).listen(port);
+/*
+
+var gpio_pin = { b: 11, g: 12, r:13, b_on: false, r_on: false, g_on: false };
 var gpio = require('rpi-gpio');
+
 gpio.setup(gpio_pin.b, gpio.DIR_OUT, (err) => {
     if (err) {
         console.log('pin b err : ' + err);
@@ -68,6 +117,6 @@ http.createServer(function (req, res) {
         res.write(html);
         res.end();
     }
-
 }).listen(port);
 
+*/
