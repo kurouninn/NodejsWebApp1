1672c66a0ef4fc3bd9205a8794683c4b728fcd81
yi <yi@hus.ac.jp>
2019-01-19 12:38:50 +0900

    1

---
+++ NodejsWebApp1/test.js
@@ -0,0 +1,10 @@
+﻿
+var HLSServer = require('hls-server')
+var http = require('http')
+
+var server = http.createServer()
+var hls = new HLSServer(server, {
+    path: '/',     // Base URI to output HLS streams
+    dir: 'live'  // Directory that input files are stored
+})
+server.listen(1935)
